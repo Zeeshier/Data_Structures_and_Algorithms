6fff26089d2f495e08b1a68826d028a672f49f1f
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2024-08-24 23:30:21 -0700

    Recursion

--- Java/2. Recursion/FactorialRecursion.java
+++ Java/2. Recursion/FactorialRecursion.java
@@ -1,19 +1,19 @@
 public class FactorialRecursion {
-    public static void main(String[] args) {
-        System.out.println("Factorial Calculation");
-
-        int number = 5; 
-        int result = factorial(number);
-
-        System.out.println("Factorial of " + number + " is: " + result);
-    }
-
-    public static int factorial(int n) {
-        // Base case: factorial of 0 is 1
-        if (n == 0) {
-            return 1;
+        public static void main(String[] args) {
+            System.out.println("Factorial Calculation");
+    
+            int number = 5; 
+            int result = factorial(number);
+    
+            System.out.println("Factorial of " + number + " is: " + result);
         }
-        // Recursive call to calculate factorial
-        return n * factorial(n - 1);
+    
+        public static int factorial(int n) {
+            // Base case: factorial of 0 is 1
+            if (n == 0) {
+                return 1;
+            }
+            // Recursive call to calculate factorial
+            return n * factorial(n - 1);
+        }
     }
-}

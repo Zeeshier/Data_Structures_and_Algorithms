93414d43d7d64ab5c95e68c34f4bacb7d29d019d
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2024-08-25 20:47:17 -0700

    Sorting & Searching

---
+++ Java/1. Sorting & Searching/Searching/BinarySearch.java
@@ -0,0 +1,41 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        System.out.println("Binary Search");
+        int arr[] = {1, 2, 3, 4, 5}; 
+        int target = 4; // Value to search for
+
+        int result = binarySearch(arr, target);
+
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if target is present at mid
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            // If target is greater, ignore left half
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            // If target is smaller, ignore right half
+            else {
+                right = mid - 1;
+            }
+        }
+
+        // Target is not present in array
+        return -1;
+    }
+}

141538828baf1609d8f3e9988646a0b175be9f65
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2024-08-31 00:47:01 -0700

    Graph

--- Java/6. Graph/AdjacencyList.java
+++ Java/6. Graph/AdjacencyList.java
@@ -1,23 +1,25 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class AdjacencyList{
-    private LinkedList<Integer>[] adjacencyList;
+public class AdjacencyList {
+    private ArrayList<LinkedList<Integer>> adjacencyList;
     private int numVertices;
 
     // Parameterized constructor
     public AdjacencyList(int numVertices) {
         this.numVertices = numVertices;
-        adjacencyList = new LinkedList[numVertices];
+        this.adjacencyList = new ArrayList<>(numVertices);
+
         for (int i = 0; i < numVertices; i++) {
-            adjacencyList[i] = new LinkedList<>();
+            adjacencyList.add(new LinkedList<>());
         }
     }
 
     // Method to add an edge to the graph
     public void addEdge(int src, int dest) {
         if (src >= 0 && src < numVertices && dest >= 0 && dest < numVertices) {
-            adjacencyList[src].add(dest);
-            adjacencyList[dest].add(src); // For undirected graph
+            adjacencyList.get(src).add(dest);
+            adjacencyList.get(dest).add(src); // For undirected graph
         } else {
             System.out.println("Invalid edge!");
         }
@@ -27,7 +29,7 @@ public class AdjacencyList{
     public void displayGraph() {
         for (int i = 0; i < numVertices; i++) {
             System.out.print("Vertex " + i + ":");
-            for (Integer vertex : adjacencyList[i]) {
+            for (Integer vertex : adjacencyList.get(i)) {
                 System.out.print(" -> " + vertex);
             }
             System.out.println();
